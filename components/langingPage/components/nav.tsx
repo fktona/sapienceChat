ed30943c86978585dd17e5f784592a30860d4e07
fktona <adetona.fk@gmail.com>
2024-05-15 08:33:23 -0700

    [completed]

--- components/langingPage/components/nav.tsx
+++ components/langingPage/components/nav.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 export default function 
 Nav() {
   return (
-    <nav className='   text-white/90 flex justify-between px-4
-     mb-12 text-[18px] relative'>
+    <nav className='   text-white/90 flex justify-between lg:px-4
+     mb-12 lg:text-[18px] text-[15px] relative'>
         <ul className='flex items-center gap-4'>
             <li>Solutions</li>
             <li>Pricing</li>
         </ul>
-        <div className='font-bold text-[25px]'>
+        <div className='font-bold text-[20px] lg:text-[25px]'>
             <span>LOGO</span>
         </div>
-        <button>
+        <button className='hidden md:block'>
             sign in
         </button>
     </nav>
